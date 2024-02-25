37adefa690157ecc944eb67d88dc14556ad6b1fd
ringo9971 <e@DESKTOP-S224KGH>
2024-02-25 17:11:56 +0900

    :sparkles: Cors を追加

--- test-api/src/main.rs
+++ test-api/src/main.rs
@@ -1,7 +1,7 @@
-use std::net::SocketAddr;
-
-use actix_web::{web, App, HttpServer};
+use actix_cors::Cors;
+use actix_web::{http::header, web, App, HttpServer};
 use dotenv::dotenv;
+use std::{env, net::SocketAddr};
 use test_api::{
     api::users::{create_user, delete_user, get_users, update_user},
     error::Error,
@@ -12,9 +12,17 @@ async fn main() -> Result<(), Error> {
     dotenv().ok();
 
     let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
+    let allowed_origin = env::var("APP_BASE_URL").expect("failed get env var (APP_BASE_URL)");
 
-    HttpServer::new(|| {
+    HttpServer::new(move || {
         App::new()
+            .wrap(
+                Cors::default()
+                    .allowed_origin(&allowed_origin)
+                    .allowed_methods(vec!["GET", "POST", "PUT", "DELETE"])
+                    .allowed_header(header::CONTENT_TYPE)
+                    .max_age(3600),
+            )
             .route("/users", web::post().to(create_user))
             .route("/users", web::get().to(get_users))
             .route("/users/{user_uid}", web::put().to(update_user))
