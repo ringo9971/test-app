ccd20451b855a5f9ed99b7249daf34669fbd91e8
ringo9971 <e@DESKTOP-S224KGH>
2024-02-25 14:58:45 +0900

    :art: prettier を実行

--- test-front/src/App.tsx
+++ test-front/src/App.tsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 function App() {
   return (
     <>
       <h1>test-app</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
